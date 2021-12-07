da782194e70977e4f438e6eb6a01001670a80931
Danielto1404 <daniilkorolev00@gmail.com>
2021-12-07 09:50:15 +0300

    Delete train.sh

--- segmentation/eyes/train.sh
+++
@@ -1,9 +0,0 @@
-python3 train.py --annotation_path=/Users/a19378208/Documents/GitHub/Generative-Makeup/data/50-labels.json \
-                 --images_root=/Users/a19378208/Documents/GitHub/Generative-Makeup/data/images \
-                 --model_path=checkpoints/model.pth \
-                 --num_classes=3 \
-                 --num_epochs=10 \
-                 --batch_size=4 \
-                 --lr=0.001 \
-                 --l2=0.001 \
-                 --device=cpu
