90b1bd14c8258ff21fa065fd420700d1115c0fc7
Danielto1404 <daniilkorolev00@gmail.com>
2021-12-07 23:46:46 +0300

    update criterion bug + fcn

--- segmentation/eyes/model.py
+++ segmentation/eyes/model.py
@@ -1,18 +1,18 @@
 import torch
 import torch.nn as nn
-from torchvision.models.segmentation import deeplabv3_resnet50, deeplabv3_resnet101
+from torchvision.models.segmentation import fcn_resnet50, deeplabv3_resnet50, deeplabv3_resnet101
 
 from utils import torch_normalize_image
 
 arh_models = {
     'deeplabv3_resnet50': deeplabv3_resnet50,
     'deeplabv3_resnet101': deeplabv3_resnet101,
-    'unet': []
+    'fcn_resnet50': fcn_resnet50,
 }
 
 
 class SegmentationModel(nn.Module):
-    def __init__(self, num_classes: int, model_name: str = 'deeplabv3_resnet50'):
+    def __init__(self, num_classes: int, model_name: str = 'fcn_resnet50'):
         super().__init__()
         self.num_classes = num_classes
         constructor = arh_models.get(model_name)
@@ -21,13 +21,13 @@ class SegmentationModel(nn.Module):
 
         # Cached in Users/a19378208/.cache/torch/hub/checkpoints/resnet50-0676ba61.pth
         self.model = constructor(num_classes=num_classes)
-        self.criterion = nn.CrossEntropyLoss(weight=torch.tensor([0.1, 0.45, 0.45]))
 
     def forward(self, image, mask=None) -> dict:
         image = torch_normalize_image(image)
         outputs = self.model(image)
         if mask is not None:
+            criterion = nn.CrossEntropyLoss(weight=torch.tensor([0.1, 0.45, 0.45]))
             tensors = outputs['out']
-            outputs['loss'] = self.criterion(tensors, mask)
+            outputs['loss'] = criterion(tensors, mask)
 
         return outputs
